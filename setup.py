4744c70e94155acedeefced0720c1ab9a9dd6c3f
momopranto <m.amin@nyu.edu>
2016-08-22 14:32:11 -0400

    restructured

---
+++ setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name="SafeURL",url="https://github.com/IncludeSecurity/safeurl-python",version="1.0",license="MIT License", long_description=open("README.md").read())
