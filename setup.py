e96afcfc7d7037aa533dbecd41147d219a8035c0
Jordan Milne <jordan.milne@saynotolinux.com>
2016-10-25 23:31:45 -0700

    Fix requirements

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from distutils.core import setup
 
-setup(name="SafeURL",url="https://github.com/IncludeSecurity/safeurl-python",version="1.0",license="MIT License", long_description=open("README.md").read())
+with open("README.md") as readme_fh:
+    long_description = readme_fh.read()
+
+setup(
+    name="SafeURL",
+    url="https://github.com/IncludeSecurity/safeurl-python",
+    version="1.1",
+    license="MIT License",
+    requires=[
+        "pycurl>=7.19.3",
+    ],
+    long_description=long_description,
+)
